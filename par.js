c18bd7302e85b70e917f96a3879d409949bf1ea4
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2025-09-03 20:34:55 -0300

    commit teste

---
+++ par.js
@@ -0,0 +1,7 @@
+let im = 8
+
+if (im %2 == 0){
+    console.log(`O ${im} é par`)
+}else{
+    console.log(`O ${im} é impar`)
+}
---
+++ soma.js
@@ -0,0 +1,5 @@
+let num1 = 50
+let num2 = 21
+let soma = num1 + num2
+console.log(`A soma dos numeros ${num1} e ${num2} é ${soma}`)
+
